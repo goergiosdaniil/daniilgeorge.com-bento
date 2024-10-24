c96772a5eb0bb7ecbe3518b9a20cec4b27f131b2
goergiosdaniil <goergios2@gmail.com>
2024-10-24 17:07:52 +0300

    Testing constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
+  github: "https://github.com/goergiosdaniil",
+  linkedin: "https://www.linkedin.com/in/georgedaniil/",
   medium: "https://ladvace.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  dribble: "https://issg.gr",
 };
 
 export const loaderAnimation = [
