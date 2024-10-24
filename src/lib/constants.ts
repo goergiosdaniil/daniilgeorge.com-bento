fed6d2ae6544bf1896ed39c907bcc46f079b4b08
goergiosdaniil <goergios2@gmail.com>
2024-10-24 17:12:28 +0300

    Testing Constant link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const LINKS = {
   linkedin: "https://www.linkedin.com/in/georgedaniil/",
   medium: "https://ladvace.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://issg.gr",
+  workWebsite: "https://issg.gr",
 };
 
 export const loaderAnimation = [
